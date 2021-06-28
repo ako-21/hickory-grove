0e77bc533ccafb6c50d45526ea24d04dfcb88ee5
Akeem Ogle <aogle@hcmp.onmicrosoft.com>
2021-06-28 16:41:38 -0400

    nav resp styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
-// import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
 // import Logo from './../../images/logoplaceholder.png'
 import { BiBuildingHouse } from 'react-icons/bi'
@@ -22,30 +21,35 @@ import { BiBuildingHouse } from 'react-icons/bi'
 // )
 
 const alwaysOptions = (
-  <Fragment className="flex-row align-items-center">
+  <Fragment className="flex-row align-items-center nav-text-style">
     <Nav.Link className="mr-3" href="#/">770.123.4567</Nav.Link>
     <Nav.Link className="mr-3" href="#/">
       <BiBuildingHouse></BiBuildingHouse>
       The Space
     </Nav.Link>
     <Nav.Link>
-      <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover">
-        Book Now
+      <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover nav-button-two">
+          Book Now
       </Button>
     </Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="header" expand="lg" fixed="top" >
+  <Navbar bg="header" expand="md" fixed="top" >
     <Container style={{ paddingBottom: '.5rem', paddingTop: '.5rem' }}>
       <Navbar.Brand href="#">
         <BiBuildingHouse size={60}></BiBuildingHouse>
         HickoryGrove
       </Navbar.Brand>
+      <div className="nav-text-style">
+        <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover nav-button-one">
+          Book Now
+        </Button>
+      </div>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto flex-row align-items-center">
+        <Nav className="ml-auto flex-row align-items-center justify-content-end ">
           { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
           { alwaysOptions }
           {/* { user ? authenticatedOptions : unauthenticatedOptions } */}
