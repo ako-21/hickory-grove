c021f36f30254e15d234f2b93237bc6161ce40f6
Akeem Ogle <aogle@hcmp.onmicrosoft.com>
2021-06-30 23:03:24 -0400

    Header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
@@ -35,28 +35,64 @@ const alwaysOptions = (
   </Fragment>
 )
 
-const Header = ({ user }) => (
-  <Navbar bg="header" expand="md" fixed="top" >
-    <Container style={{ paddingBottom: '.5rem', paddingTop: '.5rem' }}>
-      <Navbar.Brand href="#">
-        <BiBuildingHouse size={60}></BiBuildingHouse>
-        HickoryGrove
-      </Navbar.Brand>
-      <div className="nav-text-style humburger-button">
-        <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover nav-button-one mr-2">
-          Book Now
-        </Button>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </div>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto flex-row align-items-center justify-content-end ">
-          { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-          { alwaysOptions }
-          {/* { user ? authenticatedOptions : unauthenticatedOptions } */}
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-)
+// const Header = ({ user }) => (
+//   <Navbar className={navbar ? 'navbar-transparent' : 'navbar-solid'} expand="md" fixed="top" >
+//     <Container style={{ paddingBottom: '.5rem', paddingTop: '.5rem' }}>
+//       <Navbar.Brand href="#">
+//         <BiBuildingHouse size={60}></BiBuildingHouse>
+//         HickoryGrove
+//       </Navbar.Brand>
+//       <div className="nav-text-style humburger-button">
+//         <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover nav-button-one mr-2">
+//           Book Now
+//         </Button>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//       </div>
+//       <Navbar.Collapse id="basic-navbar-nav">
+//         <Nav className="ml-auto flex-row align-items-center justify-content-end ">
+//           { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+//           { alwaysOptions }
+//           {/* { user ? authenticatedOptions : unauthenticatedOptions } */}
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Container>
+//   </Navbar>
+// )
+
+function Header ({ user }) {
+  const [navbar, setNavbar] = useState(true)
+
+  const changeBackground = () => {
+    if (window.scrollY >= 250) {
+      setNavbar(false)
+    } else {
+      setNavbar(true)
+    }
+  }
+  window.addEventListener('scroll', changeBackground)
+  return (
+    <Navbar className={(navbar ? 'navbar-transparent' : 'navbar-solid')} expand="md" fixed="top" >
+      <Container style={{ paddingBottom: '.5rem', paddingTop: '.5rem' }}>
+        <Navbar.Brand href="#">
+          <BiBuildingHouse size={60}></BiBuildingHouse>
+          HickoryGrove
+        </Navbar.Brand>
+        <div className="nav-text-style humburger-button">
+          <Button variant="ghost" style={{ borderWidth: '2px', borderStyle: 'solid', borderColor: '#212529' }} className="pr-4 pl-4 button-hover nav-button-one mr-2">
+            Book Now
+          </Button>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </div>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto flex-row align-items-center justify-content-end ">
+            { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+            { alwaysOptions }
+            {/* { user ? authenticatedOptions : unauthenticatedOptions } */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
 
 export default Header
