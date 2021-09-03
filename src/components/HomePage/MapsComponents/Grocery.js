4f0c61bb5db45516e28e32a89953fc0b92200580
Akeem Ogle <aogle@hcmp.onmicrosoft.com>
2021-09-03 17:31:40 -0400

    maps

--- src/components/HomePage/MapsComponents/Grocery.js
+++ src/components/HomePage/MapsComponents/Grocery.js
@@ -1,22 +1,17 @@
 import React from 'react'
-import { GoogleMap, Marker } from 'react-google-maps'
+import { GoogleMap, Marker, withScriptjs, withGoogleMap } from 'react-google-maps'
 import * as groceryData from './../../../data/data_grocery.json'
 // import ShoppingCart from './../../../images/shoppingcart.png'
 import House from './../../../images/house.png'
-// import MarkerBlue from './../../../images/markerblue.png'
-// import MarkerOrange from './../../../images/markerorange.png'
+import MarkerBlue from './../../../images/markerblue.png'
+import MarkerOrange from './../../../images/markerorange.png'
+// import Container from 'react-bootstrap/Container'
+// import Tabs from 'react-bootstrap/Tabs'
+// import Tab from 'react-bootstrap/Tab'
 
 class Grocery extends React.Component {
     state = {
       selectedStore: null
-    }
-
-    selectedStoreIn = (store) => {
-      this.setState({ selectedStore: store })
-    }
-
-    selectedStoreOut = () => {
-      this.setState({ selectedStore: null })
     }
 
     render () {
@@ -26,29 +21,69 @@ class Grocery extends React.Component {
             position= {{ lat: 34.06425, lng: -84.60060 }}
             icon={{
               url: `${House}`,
-              scaledSize: new window.google.maps.Size(50, 46)
+              scaledSize: new window.google.maps.Size(55, 55)
             }}
           >
           </Marker>
-          {groceryData.results.map((store, index) => (
-            <Marker
-              key={store.place_id}
-              onMouseOver= {this.selectedStoreIn}
-              onMouseOut= {this.selectedStoreOut}
-              position= {{ lat: store.geometry.location.lat, lng: store.geometry.location.lng }}
-              label= {{
-                text: `${index + 1}`,
-                color: 'white'
-              }}
-              icon={{
-                scaledSize: new window.google.maps.Size(40, 50)
-              }}
-            >
-            </Marker>
-          ))}
+          {groceryData.results.map((store, index) => {
+            return this.state.selectedStore === store.place_id
+              ? (
+                <Marker
+                  key={store.place_id}
+                  onMouseOver= {() => this.setState({ selectedStore: store.place_id })}
+                  onMouseOut= {() => this.setState({ selectedStore: null })}
+                  position= {{ lat: store.geometry.location.lat, lng: store.geometry.location.lng }}
+                  label= {{
+                    text: `${index + 1}`,
+                    color: 'white'
+                  }}
+                  icon={{
+                    url: `${MarkerBlue}`,
+                    scaledSize: new window.google.maps.Size(40, 45)
+                  }}
+                >
+                </Marker>
+              ) : (
+                <Marker
+                  key={store.place_id}
+                  onMouseOver= {() => this.setState({ selectedStore: store.place_id })}
+                  onMouseOut= {() => this.setState({ selectedStore: null })}
+                  position= {{ lat: store.geometry.location.lat, lng: store.geometry.location.lng }}
+                  label= {{
+                    text: `${index + 1}`,
+                    color: 'white'
+                  }}
+                  icon={{
+                    url: `${MarkerOrange}`,
+                    scaledSize: new window.google.maps.Size(40, 45)
+                  }}
+                >
+                </Marker>
+              )
+          }
+          )}
         </GoogleMap>
       )
     }
 }
 
-export default Grocery
+class GroceryRender extends React.Component {
+  state ={
+  }
+  render () {
+    const WrappedMap = withScriptjs(withGoogleMap(Grocery))
+    return (
+      <div className="mb-5">
+        <div style={{ width: 'auto', height: '518.09px' }}>
+          <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
+            loadingElement={<div style={{ height: '100%' }} />}
+            containerElement={<div style={{ height: '100%', width: '100%' }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GroceryRender
--- src/components/HomePage/Section6.js
+++ src/components/HomePage/Section6.js
@@ -1,26 +1,24 @@
 import React from 'react'
+import GroceryRender from './MapsComponents/Grocery'
+import GroceryTab from './MapsComponents/GroceryTab'
 import Container from 'react-bootstrap/Container'
-import Grocery from './MapsComponents/Grocery'
-import { withScriptjs, withGoogleMap } from 'react-google-maps'
 
 class Section6 extends React.Component {
   state = {
-
+    test: false
   }
 
   render () {
-    const WrappedMap = withScriptjs(withGoogleMap(Grocery))
     return (
-      <Container className="mb-5">
-        <Container className="pt-5">
-          <h1 className="font3" style={{ color: '#214d67', fontWeight: 'bold' }}>See What&apos;s Around</h1>
-        </Container>
-        <div style={{ width: 'auto', height: '100vh' }}>
-          <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '100%' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
+      <Container>
+        <h1 className="font3 mt-5 mb-5" style={{ color: '#214d67', fontWeight: 'bold' }}>See What&apos;s Around</h1>
+        <div className="row no-gutters">
+          <div className="col-5">
+            <GroceryRender {...this.state}></GroceryRender>
+          </div>
+          <div className="col-7">
+            <GroceryTab></GroceryTab>
+          </div>
         </div>
       </Container>
     )
